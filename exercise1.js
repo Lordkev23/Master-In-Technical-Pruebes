429a6c98019db3236bbb2062983c2ccb036fbe05
Kevin <kevdaviman.23@gmail.com>
2023-04-11 11:45:48 -0500

    added statement

---
+++ exercise1.js
@@ -0,0 +1,21 @@
+/*
+Given a Marvel superhero object, write a program that can shows information about a superhero.
+
+It also adds the ability to search the information of many superheroes at once.
+
+Example 1:
+showSuperHeroInformation('Iron Man');
+
+out:
+Real name: Tony Stark
+Powers: Advanced Technology, Air Mobility
+Team: The Avengers
+
+Example2:
+showSuperHerosInformation([array of names]);
+
+out:
+Shows the information of all superheroes.
+
+*/
+
