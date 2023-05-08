e48d7617210749d9bff248bbc8a9a8444c016faf
Kevin <kevdaviman.23@gmail.com>
2023-05-08 12:14:40 -0500

    added Statement

---
+++ exercise4.js
@@ -0,0 +1,14 @@
+/* 
+Statement:
+    Make ahorcado game.
+    The user will have to guess and discover the letters of a secret word, and will have 5 attempts
+    (An attempt per limb of the human body).
+
+    Useful data input methode usual in your language, in the caso of JS, we use the function prompt.
+
+    Example:
+            INPUT                                          OUTPUT
+    ahorcadoGame('kevin');                          Guess the letter: e
+                                                    the word is: _e___
+                                                    You get 5 attemps.
+*/
