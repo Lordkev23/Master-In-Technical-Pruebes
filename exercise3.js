6d4b95c8e45f2e011e4a4e849fc904ff944355c3
Kevin <kevdaviman.23@gmail.com>
2023-05-07 22:24:02 -0500

    comments added

--- exercise3.js
+++ exercise3.js
@@ -8,16 +8,17 @@ differenceInDays('Dec 1, 2023', 'Dec 24, 2023');                   23
 
 const differenceInDays = (date_1, date_2) => {
 
-    //
+    //Given the correct format to the date
     date_1 = date_1 instanceof Date ? date_1 : new Date(date_1);
     date_2 = date_2 instanceof Date ? date_2 : new Date(date_2);
 
-    //
+    //Get the difference in milliseconds between both dates
     const millisecondsDifference = Math.abs(date_1.getTime() - date_2.getTime());
 
-    //
+    //Divide the difference in milliseconds by the number of milliseconds in a day and round to get the days
     const days = Math.floor(millisecondsDifference / (1000 * 60 * 60 * 24));
 
+    //In case a NaN appears
     if(isNaN(days)) 'wrong date format, please use the american format';
 
     return `The difference are ${days} days`;
